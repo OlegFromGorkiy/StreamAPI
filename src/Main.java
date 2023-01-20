7d654ae8159a6e4c2fbe3307bad92c1d491496cc
Oleg Kartashev <officinator@gmail.com>
2023-01-20 19:31:43 +0300

    my solution

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<Integer> integers = List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+        task2(integers);
+    }
+
+    private static <T> void task1(Stream<? extends T> stream,
+                                  Comparator<? super T> order,
+                                  BiConsumer<? super T, ? super T> minMaxConsumer) {
+        minMaxConsumer.accept(
+                stream.min(order).orElse(null),
+                stream.max(order).orElse(null));
+    }
+
+    private static void task2(List<Integer> list) {
+        System.out.println(list.stream().filter(i -> i % 2 == 0).count());
     }
 }
